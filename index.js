76b88ab6f507b38b26c7a790198c40731b8599ec
antleemoore <antleemoore@hotmail.com>
2020-12-26 00:44:25 -0500

    testing DB

--- index.js
+++ index.js
@@ -9,11 +9,14 @@ const express = require('express'),
 
 require('dotenv').config();
 mongoose
-	.connect(process.env.DB_URL, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-	})
+	.connect(
+		'mongodb+srv://admin:CtGwFp6J7eNpbtZU@products-cluster.jr2jn.mongodb.net/products?retryWrites=true&w=majority',
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		}
+	)
 	.then(() => {
 		console.log('connection open');
 	})
